963c5b4475473f4a796e89541f2b2f02d4948d4c
Samuel <JeanCor19>
2022-05-17 03:07:01 -0500

    Desde mi laptop

--- CORTEZTORRESJEANPIERRE/ACTIVIDAD-B2-C2/CORTEZTORRESJEANPIERRE-cuentamoneda.cpp
+++ CORTEZTORRESJEANPIERRE/ACTIVIDAD-B2-C2/CORTEZTORRESJEANPIERRE-cuentamoneda.cpp
@@ -1,31 +1,45 @@
+//==================================================
+//==>Nombre del programa: cuentamoneda
+//==>Autor: CORTEZ TORRES JEAN PIERRE
+//==> Archivo: CORTEZTORRESJEANPIERRE-cuentamoneda.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	int n, c=0, c1=0, c2=0;
-	float x, a=0, a1=0, a2=0, m=0.10;
+	int ct_n, ct_c=0, ct_c1=0, ct_c2=0;
+	float ct_x, ct_a=0, ct_a1=0, ct_a2=0, ct_m=0.10;
 	cout<<"Ingrese la cantida de monedas: ";
-	cin>>n;
+	cin>>ct_n;
 	do{
 	cout<<"Ingrese cual es el valor de la moneda (0.10,0.25) : ";
-	cin>>x;
-	c=c+1;
-	a=a+x;
-	if(x==m){
-		c1=c1+1;
-		a1=a1+x;
+	cin>>ct_x;
+	ct_c=ct_c+1;
+	ct_a=ct_a+ct_x;
+	if(ct_x==ct_m){
+		ct_c1=ct_c1+1;
+		ct_a1=ct_a1+ct_x;
 	}else{
-		c2=c2+1;
-		a2=a2+x;
+		ct_c2=ct_c2+1;
+		ct_a2=ct_a2+ct_x;
 	}
-	}while(c<n);
+	}while(ct_c<ct_n);
+//==================================================
+//==>Nombre del programa: cuentamoneda
+//==>Autor: CORTEZ TORRES JEAN PIERRE
+//==> Archivo: CORTEZTORRESJEANPIERRE-cuentamoneda.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
 	cout<<"El total de monedas fue : "<<endl;
-	cout<<"Cantidad de monedas ingresadas : "<<c<<endl;
-	cout<<"El total del dinero contado es de : "<<a<<endl;
-	cout<<"Monedas ingresadas de 0.10 ctvos es de :"<<c1<<endl;
-	cout<<"Cantidad total de monedas de 0.10 ctvos es de : "<<a1<<endl;
-	cout<<"Monedas ingresadas de 0.25 ctvos es de : "<<c2<<endl;
-	cout<<"Cantidad total de monedas de 0.25 ctvos es de : "<<a2<<endl;
+	cout<<"Cantidad de monedas ingresadas : "<<ct_c<<endl;
+	cout<<"El total del dinero contado es de : "<<ct_a<<endl;
+	cout<<"Monedas ingresadas de 0.10 ctvos es de :"<<ct_c1<<endl;
+	cout<<"Cantidad total de monedas de 0.10 ctvos es de : "<<ct_a1<<endl;
+	cout<<"Monedas ingresadas de 0.25 ctvos es de : "<<ct_c2<<endl;
+	cout<<"Cantidad total de monedas de 0.25 ctvos es de : "<<ct_a2<<endl;
 	return (0);
 }
