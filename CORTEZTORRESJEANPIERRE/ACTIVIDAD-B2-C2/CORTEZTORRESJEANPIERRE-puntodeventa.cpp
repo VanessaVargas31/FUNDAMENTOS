e070156f0d729c4ac0b92f068f3b0ee0c45ff346
Samuel <JeanCor19>
2022-05-17 03:45:36 -0500

    Desde mi laptop

--- CORTEZTORRESJEANPIERRE/ACTIVIDAD-B2-C2/CORTEZTORRESJEANPIERRE-puntodeventa.cpp
+++ CORTEZTORRESJEANPIERRE/ACTIVIDAD-B2-C2/CORTEZTORRESJEANPIERRE-puntodeventa.cpp
@@ -1,29 +1,45 @@
+
+//==================================================
+//==>Nombre del programa: puntodeventa
+//==>Autor: CORTEZ TORRES JEAN PIERRE
+//==> Archivo: CORTEZTORRESJEANPIERRE-puntodeventa.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
+
 #include <iostream>
 using namespace std;
 int main ()
 {
-int c=0, P;
-float A=0, x, Tb, Piva, Pdsc, iva=0.12, desc=0.30, vt;
-cout<<"Ingrese la cantidad de productos: ";
-cin>>P;
+int ct_c=0, ct_P;
+float ct_A=0, ct_x, ct_Tb, ct_Piva, ct_Pdsc, ct_iva=0.12, ct_desc=0.30, ct_vt;
+cout<<"Ingrese la cantidad de productos: "; 
+cin>>ct_P;
 do{
 cout<<"Ingrese el valor de los productos: ";
-cin>>x;
-c=c+1;
-A=A+x;
-Piva=A*iva;
-cout<<"El valor del iva de la compra es de: $"<<Piva<<endl;
-Pdsc=A*desc;
-cout<<"El valor del descuento de su compra es de: $"<<Pdsc<<endl;
-Tb=Piva+Pdsc;
-}while (c<P);
-if (Tb==A){
-cout<<"Su compra aplica para un descuento:"<<Tb<<endl;
-iva=Tb*Piva/100;
-desc=Tb*Pdsc/100;
+cin>>ct_x;
+ct_c=ct_c+1;
+ct_A=ct_A+ct_x;
+ct_Piva=ct_A*ct_iva;
+cout<<"El valor del iva de la compra es de: $"<<ct_Piva<<endl;
+ct_Pdsc=ct_A*ct_desc;
+cout<<"El valor del descuento de su compra es de: $"<<ct_Pdsc<<endl;
+ct_Tb=ct_Piva+ct_Pdsc;
+}while (ct_c<ct_P);
+if (ct_Tb==ct_A){
+cout<<"Su compra aplica para un descuento:"<<ct_Tb<<endl;
+ct_iva=ct_Tb*ct_Piva/100;
+ct_desc=ct_Tb*ct_Pdsc/100;
 }else{
-vt=A+Piva-Pdsc;
-cout<<"El valor total es de: $"<<vt<<endl;
+ct_vt=ct_A+ct_Piva-ct_Pdsc;
+//==================================================
+//==>Nombre del programa: puntodeventa
+//==>Autor: CORTEZ TORRES JEAN PIERRE
+//==> Archivo: CORTEZTORRESJEANPIERRE-puntodeventa.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
+cout<<"El valor total es de: $"<<ct_vt<<endl;
 }
 return(0);
 }
--- CORTEZTORRESJEANPIERRE/ACTIVIDAD-B2-C2/CORTEZTORRESJEANPIERRE-sumadevariosnumeros.cpp
+++ CORTEZTORRESJEANPIERRE/ACTIVIDAD-B2-C2/CORTEZTORRESJEANPIERRE-sumadevariosnumeros.cpp
@@ -1,21 +1,35 @@
+//==================================================
+//==>Nombre del programa: sumadevariosnumeros
+//==>Autor: CORTEZ TORRES JEAN PIERRE
+//==> Archivo: CORTEZTORRESJEANPIERRE-sumadevariosnumeros.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int c=0, n;
-    float s=0, x;
-    cout<<"Ingrese la cantidad de numeros a sumar:";
-    cin>>n;
+    int ct_c=0,ct_n;
+    float ct_s=0, ct_x;
+    cout<<"Ingrese la cantidad de números a sumar:";
+    cin>>ct_n;
     do{
   
-    cout<<"Ingrese un numero:";
-    cin>>x;
-    c=c+1;
-    s=s+x;
-       } while(c<n);
-            cout<<"Ingrese un numero para sumarlo: "<<s<<endl;
+    cout<<"Ingrese un número:";
+    cin>>ct_x;
+    ct_c=ct_c+1;
+    ct_s=ct_s+ct_x;
+       } while(ct_c<ct_n);
+//==================================================
+//==>Nombre del programa: sumadevariosnumeros
+//==>Autor: CORTEZ TORRES JEAN PIERRE
+//==> Archivo: CORTEZTORRESJEANPIERRE-sumadevariosnumeros.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
+            cout<<"Ingrese un número  para sumarlo: "<<ct_s<<endl;
         
         return(0);
 }
