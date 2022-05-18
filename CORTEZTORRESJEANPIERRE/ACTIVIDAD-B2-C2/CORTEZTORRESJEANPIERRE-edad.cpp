2518bec2a0f0c659daf2e010b5e4e2ce9f9315c9
Samuel <JeanCor19>
2022-05-17 23:30:59 -0500

    Desde mi laptop

--- CORTEZTORRESJEANPIERRE/ACTIVIDAD-B2-C2/CORTEZTORRESJEANPIERRE-edad.cpp
+++ CORTEZTORRESJEANPIERRE/ACTIVIDAD-B2-C2/CORTEZTORRESJEANPIERRE-edad.cpp
@@ -24,19 +24,21 @@ int main ()
 	}
 	if(ct_MA>=ct_MN){
 	ct_M=ct_MA-ct_MN;
+
 	}else{
 	ct_MN=ct_MN+12;
 	ct_AA=ct_AA-1;
 	ct_M=ct_MA-ct_MN;
 	}
 	ct_A=ct_AA-ct_AN;
-//==================================================
-//==>Nombre del programa: edad
-//==>Autor: CORTEZ TORRES JEAN PIERRE
-//==> Archivo: CORTEZTORRESJEANPIERRE-edad.cpp
-//==>Fecha de elaboración: 2022-04-01
-//==>Fecha ultima actualización: 2022-05-12
-//==================================================
+
+
+	cout<<"//=====Nombre del programa: edad "<<endl;
+        cout<<"//=====Archivo: CORTEZTORRESJEANPIERRE-edad.cpp"<<endl;
+        cout<<"//=====Autor: CORTEZ JEAN PIERRE "<<endl;
+        cout<<"//=====Fecha de elaboración: 30-04-2022"<<endl;
+        cout<<"//=====Fecha de ultima actualización: 17-05-2022"<<endl;
+
 	cout<<"Usted tiene "<<ct_A<<" años "<<ct_M<<" meses  y"<<ct_D<<" dias ";
 	return(0);
 }
