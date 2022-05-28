01a4154f32c7b1e64d77b3c24aefadc3186d6470
Cheeto333 <101517128+Cheeto333@users.noreply.github.com>
2022-05-27 20:57:54 -0500

    REPOSTEADO

---
+++ ARTUROKEVIN/SumaDeDosNumeros.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a, b, c;
+	cout<<"Ingrese 2 numeros para realizar la suma"<<endl;
+	cin>>a>>b;
+	c=a+b;
+	cout<<"El resultado es:"<<endl;
+	cout<<c;
+	return 0;
+}
